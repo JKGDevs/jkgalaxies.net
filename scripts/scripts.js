da4c6ec1f79bf0ee4f13079dcffd5c397aed4527
DarthFutuza <darthfutuza@hotmail.com>
2019-10-09 09:32:57 -0600

    fixed 404 loading, much better way of doing it now

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -41,12 +41,12 @@ window.onload = function()
 
     if($('body').hasClass("404Page"))
     {
-        var random = Math.floor(Math.random() * jQuery('.ErrorJoke').length);
-        jQuery('.ErrorJoke').hide().eq(random).show();
+        select404Error();
     }
 
     if ((window.innerHeight > window.innerWidth) && ($(window).width() < 768)) 
     {
+        
         $(".maindiv").css("margin", "0%");
         $(".centerdivbig").css("margin", "0%");
         $(".centerdiv").css("margin", "0%");
@@ -137,6 +137,19 @@ function loadNavBar()
         }
 }
 
+/* randomly choose a 404 error message to display*/
+function select404Error()
+{
+    //if you add new 404 error jokes, each array needs to stay the same size
+    var fofimages = ["https://i.imgur.com/TwgFzdm.gif", "https://i.imgur.com/JlliXwt.gif", "https://i.imgur.com/6fWJhKw.gif"];      //holds image
+    var fofalt = ["Move along.  Move along.", "Perhaps the archives are incomplete...", "No ALderaan?"];                            //holds the alt text for the image
+    var foftext =  ["This is not the page you're looking for.", "If an item does not appear in our records, it does not exist.", "It's not on any of the charts."]; //holds the title text
+    var select = Math.floor(Math.random() * fofimages.length);
+    
+    $("#TextJoke").text(foftext[select]);
+    $("#ImgJoke").attr("src",fofimages[select]);
+    $("#ImgJoke").attr("alt", fofalt[select]);
+}
 
 /*load a text file into html document*/
 function loadText(tagID, fileLocation)
